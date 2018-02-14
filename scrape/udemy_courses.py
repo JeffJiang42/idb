d970f74b8357e208dc39176847f5ab106553d8c4
kurtisdavid <kurtis.david@live.com>
2018-02-13 20:30:16 -0600

    added comments + increase data / page

--- scrape/udemy_courses.py
+++ scrape/udemy_courses.py
@@ -4,18 +4,36 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 def getUdemyDescription(url):
+    '''
+    Scrape the description not found in api
+    Param:
+        url - url of the udemy course
+    Return:
+        description - extracted headline
+    '''
+    # pretend we're not robots
     user_header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}
     url = 'http://udemy.com' + url
     req = requests.get(url,headers = user_header)
     soup = BeautifulSoup(req.text,'lxml')
-    
+    # steal the headline as description
     description = soup.body.find("div",class_ = "clp-lead__headline").text
     return description.strip()
     
 
 def getUdemyInfo(entry,udemy_info):
+    '''
+    Given a course from Udemy API, extract info and save in dict
+    Param:
+        entry - json dict containing api information from udemy
+        udemy_info - python dict containing aggregate information
+    Return:
+        null
+    '''
+    # should only see courses
     assert entry['_class'] == 'course'
     
+    # get the information
     udemy_info['Course'].append(entry['title'])
     udemy_info['Price'].append(entry['price'])
     udemy_info['Link'].append(entry['url'])
@@ -25,7 +43,9 @@ def getUdemyInfo(entry,udemy_info):
     udemy_info['Description'].append(getUdemyDescription(entry['url']))
     
 if __name__ == "__main__":
-    url = "https://www.udemy.com/api-2.0/courses/"
+    # max entries 100 / page
+    url = "https://www.udemy.com/api-2.0/courses/?page=1&page_size=100"
+    # data obtainable from api
     udemy_info = {
         'Course': [],
         'Price': [],
@@ -34,21 +54,26 @@ if __name__ == "__main__":
         'Link': [],
         'Image': []
     }
+    # api key (don't steal please)
     headers = {"Authorization": "Basic ak9uRVliaFh1eHlKeFl4UXl0WTEzcm1MWHF5a01ldk96QldqNFZiTjpTZUtienJESFdyd3UyUEEzaWI3UDgwanBZYlVZd2pCMGdHcDZ2OGowVmVjNmdVc3dGd1NvRkxHWjZRYVVFZHI2NldHdnhKdzZTdDFzbkRWRzA0aU9wb0NvbDVFbkxORE1wQWNlRkJ1bnl2OUJxQ3Z6NmJtRWthbGplOGJIakNIYw==",
     "Accept": "application/json, text/plain, */*"}
     while True:
         try:
+            # progress
             print(url)
+            # scrape and format for us to pass in
             r = requests.get(url,headers = headers)
             soup= BeautifulSoup(r.text,'lxml')
             url = json.loads(soup.body.p.text)['next']
             results = json.loads(soup.body.p.text)['results']
+            # extract info
             for entry in results:
                 getUdemyInfo(entry,udemy_info)
         except Exception as e:
+            # uh oh
             print(e)
             break
-    
+    # save to csv
     df = pd.DataFrame.from_dict(udemy_info)
     df.to_csv("udemy_courses.csv")
             
