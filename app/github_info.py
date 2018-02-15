595444cd347df7a4b3c72477790102894761b903
jeffjiang42 <jeffjiang@utexas.edu>
2018-02-15 13:18:44 -0600

    fixed commit tracking

--- app/github_info.py
+++ app/github_info.py
@@ -7,36 +7,30 @@ def getCommits():
 
     if r.status_code == 200:
         #successful call
+        jeff_commits = 0
+        kurtis_commits = 0
+        william_commits = 0
+        brandon_commits = 0
+        spencer_commits = 0
 
         info = r.json()
         for contributor in info:
-            jeff_commits = 0
-            kurtis_commits = 0
-            william_commits = 0
-            brandon_commits = 0
-            spencer_commits = 0
-
             info = r.json()
             for contributor in info:
                 if contributor['author']['login'] == 'JeffJiang42':
-                    for week  in contributor['weeks']:
-                        jeff_commits += week['c']
+                    jeff_commits = contributor['total']
 
                 if contributor['author']['login'] == 'kurtisdavid':
-                    for week in contributor['weeks']:
-                        kurtis_commits += week['c']
+                    kurtis_commits = contributor['total']
 
                 if contributor['author']['login'] == 'Acciaccatura':
-                    for week in contributor['weeks']:
-                        william_commits += week['c']
+                    william_commits = contributor['total']
 
                 if contributor['author']['login'] == 'bchan565':
-                    for week in contributor['weeks']:
-                        brandon_commits += week['c']
+                    brandon_commits = contributor['total']
 
                 if contributor['author']['login'] == 'spencerhuff':
-                    for week in contributor['weeks']:
-                        spencer_commits += week['c']
+                    spencer_commits = contributor['total']
 
     total = jeff_commits + kurtis_commits + william_commits + brandon_commits + spencer_commits
     return jeff_commits, kurtis_commits, william_commits, brandon_commits, spencer_commits, total
