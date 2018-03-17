cb5f6be70761aab4d9ead5da1848b9fa881c9104
jeffjiang42 <jeffjiang@utexas.edu>
2018-03-17 16:37:14 -0500

    fixed about page link, navbar links, and set up testing framework

---
+++ react/__tests__/navbar.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { expect } from 'chai';
+import Enzyme from 'enzyme';
+import { mount, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
+
+import MyNavbar from '../src/components/MyNavbar';
+import NavItem from 'react-bootstrap';
+
+describe('Navigation Component', () => {
+  it('renders logo', () => {
+    const navbar = mount(<MyNavbar />)
+    expect(navbar.find('img')).to.have.length(1)
+  })
+  it('4 links rendered', () => {
+    const navbar = mount(<MyNavbar />)
+    expect(navbar.find('NavItem')).to.have.length(4)
+  })
+})
--- react/src/components/MyNavbar.jsx
+++ react/src/components/MyNavbar.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Navbar, NavItem, Nav, Image } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 import './styles/MyNavbar.css';
 
 class MyNavbar extends Component{
@@ -9,27 +9,26 @@ class MyNavbar extends Component{
       <Navbar inverse fluid>
         <Navbar.Header>
           <div className="brand-align">
+          <Router>
           <Navbar.Brand>
-            <Link to="/"><Image src="https://i.imgur.com/g16hr23.png"/></Link>
+              <a href="/"><Image src="https://i.imgur.com/g16hr23.png"/></a>
           </Navbar.Brand>
+          </Router>
         </div>
           <Navbar.Toggle />
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav pullRight>
-            <NavItem eventKey={1} componentClass={Link} href="/" to="/">
-              Home
-            </NavItem>
-            <NavItem eventKey={2} componentClass={Link} href="/about" to="/about">
+            <NavItem eventKey={2} href="/about" to="/about">
               About
             </NavItem>
-            <NavItem eventKey={2} componentClass={Link} href="/courses" to="/courses">
+            <NavItem eventKey={2} href="/courses" to="/courses">
               Courses
             </NavItem>
-            <NavItem eventKey={2} componentClass={Link} href="/"subjects to="/subjects">
+            <NavItem eventKey={2} href="/"subjects to="/subjects">
               Subjects
             </NavItem>
-            <NavItem eventKey={2} componentClass={Link} href="/jobs" to="/jobs">
+            <NavItem eventKey={2} href="/jobs" to="/jobs">
               Jobs
             </NavItem>
           </Nav>
