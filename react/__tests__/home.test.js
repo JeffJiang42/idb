2940a08fcd158a2ea9fb7bb24f889d3d4b5d47fe
Brandon Chan <iambrandonchan@gmail.com>
2018-03-17 20:37:34 -0500

    home unit tests

---
+++ react/__tests__/home.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { expect } from 'chai';
+import Enzyme from 'enzyme';
+import { mount, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
+
+import Home from '../src/components/Home';
+
+describe('Home Component', () => {
+  it('contains a FrontCarousel component', () => {
+    const home = mount(<Home />);
+    expect(home.find('FrontCarousel')).to.have.length(1);
+  })
+
+  it('contains a FrontStats component', () => {
+    const home = mount(<Home />);
+    expect(home.find('FrontStats')).to.have.length(1);
+  })
+
+})
