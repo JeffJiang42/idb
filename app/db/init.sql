a0a3d9fc710941fa3c9a19d4a0f4d4da092ef91f
Acciaccatura <2899523@gmail.com>
2018-03-18 19:33:30 -0500

    course headers lowercase

--- app/db/init.sql
+++ app/db/init.sql
@@ -1,4 +1,5 @@
-DROP TABLE Subject-Job;
+DROP TABLE Subject_Job;
+DROP TABLE Course_Job;
 DROP TABLE Course;
 DROP TABLE Job;
 DROP TABLE Subject;
@@ -6,11 +7,11 @@ DROP TABLE Subject;
 CREATE TABLE Job (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(256),
-	provider VARCHAR(256),
-	image VARCHAR(256),
 	company VARCHAR(256),
+	description TEXT,
+	image VARCHAR(256),
 	link VARCHAR(256),
-	description TEXT
+	provider VARCHAR(256)
 );
 
 CREATE TABLE Subject (
@@ -40,4 +41,10 @@ CREATE TABLE Subject_Job (
 	subject_id INTEGER REFERENCES Subject(id),
 	job_id INTEGER REFERENCES Job(id),
 	PRIMARY KEY(subject_id, job_id)
+);
+
+CREATE TABLE Course_Job (
+	course_id INTEGER REFERENCES Course(id),
+	job_id INTEGER REFERENCES Job(id),
+	PRIMARY KEY(course_id, job_id)
 );
