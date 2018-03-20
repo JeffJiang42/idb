60f7e1a3d89634b9cf8d0b093cd86bab9340bc19
Spencer Huff <spencerLhuff@gmail.com>
2018-03-20 17:33:49 -0500

    updated front pages numbers to static numbers of current jobs, courses, and subjects

--- react/src/components/FrontStats.jsx
+++ react/src/components/FrontStats.jsx
@@ -9,15 +9,15 @@ class FrontStats extends Component{
         <Row>
         <div className="col statistic">
           <h3>Number of Courses</h3>
-          <h4>3</h4>
+          <h4>2772</h4>
         </div>
         <div className="col statistic">
           <h3>Number of Subjects</h3>
-          <h4>3</h4>
+          <h4>139</h4>
         </div>
         <div className="col statistic">
           <h3>Number of Jobs</h3>
-          <h4>3</h4>
+          <h4>302</h4>
         </div>
       </Row>
       </div>
