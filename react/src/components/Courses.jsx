0b9f626a8d3ad7a11a0e1fbd0b95b771077811db
jeffjiang42 <jeffjiang@utexas.edu>
2018-03-20 20:12:17 -0500

    reformmated courses to pull from dict, using dummy data until API is done

--- react/src/components/Courses.jsx
+++ react/src/components/Courses.jsx
@@ -1,16 +1,40 @@
 import CourseCard from './CourseCard.jsx';
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import _ from 'lodash'
 
 
 
 
 class Courses extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      courseList: []
+    };
+  }
+
+  componentDidMount(){
+    var data = [];
+    for (let i of _.range(10)){
+      var course = {"provider":"provider" + i, "tier": "tier " + i, "courseName":"name " + i, "image":"https://i.imgur.com/g16hr23.png"};
+      data.push(course);
+    }
+    for (let course of data){
+      course['courseRoute'] = encodeURIComponent(course["courseName"]);
+    }
+    this.setState({courseList: data});
+  }
+
   render(){
+    var courseCards = this.state.courseList.map((course) =>
+      <p>
+        <CourseCard provider={course["provider"]} tier={course["tier"]} courseRoute={course["courseRoute"]} courseName={course["courseName"]} image={course["image"]}/>
+      </p>);
     return(
-    	<p>
-    	<CourseCard provider={"insert provider"} tier={"insert tier"} courseRoute={"janitor_101"} courseName={"janitor 101"} image={"insert image"}/>
-    	</p>
+      <div>
+    	{courseCards}
+      </div>
     );
   }
 }
