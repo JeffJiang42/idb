c47678be493f8ec6f21964cf48e76b8ab89033ec
jeffjiang42 <jeffjiang@utexas.edu>
2018-03-21 00:48:56 -0500

    got courses working, need pagination

--- react/src/components/CourseCard.jsx
+++ react/src/components/CourseCard.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Router, Route, Link } from 'react-router-dom';
 import './styles/Card.css';
 import CourseData from './CourseData.jsx'
 
@@ -15,15 +15,15 @@ class CourseCard extends Component{
 	render(){
 		const { match } = this.props;
 	 	return(
-    		<div className="col-sm-3">
+    		<div className="col-sm-4">
 			  <div className="card"  align="center">
 			    <img className="card-img-top" src={this.props.image}/>
 			    <div className="card-block">
 			      <h4 className="card-title">
-			        <Link to={`/courses/${this.props.courseRoute}`}>{this.props.courseName}</Link>
+			        <Link to={`/courses/${this.props.courseId}`}>{this.props.courseName}</Link>
 			      </h4>
 			      <p className="card-text">Provider: {this.props.provider}</p>
-			      <p className="card-text">Tier: {this.props.tier}</p>
+			      <p className="card-text">Price: {this.props.price}</p>
 			    </div>
 			  </div>
 			</div>
