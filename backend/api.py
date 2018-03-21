23f818538fdcc293ec1b43f5ba97f74319fb6abd
Kurtis <kurtis.david@live.com>
2018-03-20 23:44:39 -0500

    changed description to desc as per gitbook

--- backend/api.py
+++ backend/api.py
@@ -5,8 +5,8 @@ from flask import Flask, request, Response
 
 SUBJECT_FIELDS = ('id', 'subject', 'provider', 'image', 'course-ids', 'job-ids')
 # need to add provider to gitbook
-COURSE_FIELDS = ('id', 'course', 'description', 'image', 'instructor', 'link', 'price', 'subject', 'provider', 'job-ids', 'subject-id')
-JOB_FIELDS = ('id', 'name', 'company', 'description', 'image', 'link', 'provider', 'courses', 'subject_ids')
+COURSE_FIELDS = ('id', 'course', 'desc', 'image', 'instructor', 'link', 'price', 'subject', 'provider', 'job-ids', 'subject-id')
+JOB_FIELDS = ('id', 'name', 'company', 'desc', 'image', 'link', 'provider', 'courses', 'subject_ids')
 
 FIELDS = (SUBJECT_FIELDS,COURSE_FIELDS,JOB_FIELDS)
 
