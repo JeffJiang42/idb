12d7367c565702bd94f0f70f084395bcf0022a9b
Brandon Chan <iambrandonchan@gmail.com>
2018-03-21 19:17:02 -0500

    fixed subject test, remade about test

--- react/__tests__/gui_tests/test_about.py
+++ react/__tests__/gui_tests/test_about.py
@@ -6,39 +6,28 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
-from pyvirtualdisplay import Display
 
-class UntitledTestCase(unittest.TestCase):
+class About(unittest.TestCase):
     def setUp(self):
         self.display = Display(visible=0, size=(1920, 1080))
         self.display.start()
-        self.driver = webdriver.Chrome("../linux_chrome")
+        self.driver = webdriver.Chrome('../linux_chrome')
         self.driver.implicitly_wait(30)
         self.base_url = "https://www.katalon.com/"
         self.verificationErrors = []
-        # self.accept_next_alert = True
+        self.accept_next_alert = True
     
-    def test_untitled_test_case(self):
+    def test_about(self):
         driver = self.driver
-        driver.get("http://www.learning2earn.me/")
+        driver.get("http://learning2earn.me/")
         driver.find_element_by_link_text("About").click()
         driver.find_element_by_xpath("//div[@id='root']/div/div/div[2]/div/p/a/b").click()
         driver.find_element_by_xpath("//div[@id='root']/div/div/div[2]/div/p[2]/a/b").click()
         driver.find_element_by_xpath("//div[@id='root']/div/div/div[2]/div/p[3]/a/b").click()
-        driver.find_element_by_xpath("//div[@id='root']/div/div/div[2]/div[2]/p/a/b").click()
-        driver.find_element_by_xpath("//div[@id='root']/div/div/div[2]/div[2]/p[2]/a/b").click()
-        driver.find_element_by_xpath("//div[@id='root']/div/div/div[2]/div[2]/p[5]/a/b").click()
+        driver.find_element_by_xpath("//div[@id='root']/div/div/div[2]/div[2]/p[4]/a/b").click()
         driver.find_element_by_xpath("//div[@id='root']/div/div/div[2]/div[2]/p[12]/a/b").click()
-        driver.find_element_by_xpath("//div[@id='root']/div/div/div[2]/div[3]/p/a[2]/b").click()
-        driver.find_element_by_xpath("//div[@id='root']/div/div/div/div[3]/div/div/div/h4").click()
-        driver.find_element_by_xpath("//div[@id='root']/div/div/div/div[7]/h3").click()
-        driver.find_element_by_xpath("//div[@id='root']/div/div/div/div[7]/h3").click()
-        # ERROR: Caught exception [ERROR: Unsupported command [doubleClick | //div[@id='root']/div/div/div/div[7]/h3 | ]]
-        driver.find_element_by_xpath("//div[@id='root']/div/div/div/div[3]/div/div").click()
-        driver.find_element_by_xpath("//div[@id='root']/div/div/div/div[4]/div/div/div/p[2]").click()
-        driver.find_element_by_xpath("//div[@id='root']/div/div/div/div[4]/div/div/div/p[2]").click()
-        # ERROR: Caught exception [ERROR: Unsupported command [doubleClick | //div[@id='root']/div/div/div/div[4]/div/div/div/p[2] | ]]
-        driver.find_element_by_xpath("//div[@id='root']/div/div/div/div[4]/div/div/div/p").click()
+        driver.find_element_by_xpath("//img[contains(@src,'https://i.imgur.com/g16hr23.png')]").click()
+        driver.find_element_by_link_text("About").click()
     
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
