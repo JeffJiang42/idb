af0b7b8b8a9f850dcc6d6163f6da7d651b15d67f
jeffjiang42 <jeffjiang@utexas.edu>
2018-04-05 12:55:20 -0500

    removed search from NavBar

--- react/src/components/MyNavbar.jsx
+++ react/src/components/MyNavbar.jsx
@@ -34,9 +34,6 @@ class MyNavbar extends Component{
             <NavItem eventKey={2} href="/jobs" to="/jobs">
               Jobs
             </NavItem>
-            <NavItem>
-            <Search />
-            </NavItem>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
